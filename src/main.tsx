182534fd3e69826a0f4a820c1bab81057e230cf8
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-02-11 20:39:26 +0000

    :sparkles:
    config auth login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div>
+      <a href="http://localhost:8080/login">Login on Spotify</a>
+    </div>
   </StrictMode>,
 )
